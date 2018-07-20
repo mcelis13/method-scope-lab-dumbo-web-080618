d501f660f46044cb9602adfef1cccad391126670
Madeline Celis <madelinebcelis@gmail.com>
2018-07-20 14:42:05 +0000

    automatically backed up by learn

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -1,3 +1,6 @@
 def catch_phrase(phrase)
   puts phrase
 end
+
+#changed the test spec to pass this test based on the instructions and
+# on this file I added phrase as an argument to the catch_phrase method as well
