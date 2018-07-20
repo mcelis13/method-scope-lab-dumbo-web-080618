695928cca788695722f8ae824c88f7dffe23b49a
Madeline Celis <madelinebcelis@gmail.com>
2018-07-20 14:43:12 +0000

    automatically backed up by learn

--- spec/catch_phrase_spec.rb
+++ spec/catch_phrase_spec.rb
@@ -3,6 +3,7 @@ require "spec_helper"
 describe "#catch_phrase" do
   it "puts out a catch phrase" do
     phrase = "It's-a me, Mario!"
+    #Instructed to change test spec to take phrase as an argument so that the test spec would pass
     expect{catch_phrase(phrase)}.to output("It's-a me, Mario!\n").to_stdout
   end
 end
